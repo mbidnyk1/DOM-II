7396db9796713509d183b2184833d934081ff673
Michael Bidnyk <mbidnyk1@gmail.com>
2020-03-11 16:39:53 -0500

    update

--- js/index.js
+++ js/index.js
@@ -1,4 +1,31 @@
 // Your code goes here
+
+const homeH2 = document.querySelectorAll('h2')
+document.addEventListener('scroll', () => {
+    homeH2.forEach(element => {
+        element.style.fontSize = '80px'
+    })
+})
+const input = document.querySelector('input')
+document.addEventListener('select', () => {
+        input.style.color = "purple"
+        input.style.transform = 'scale(5)'
+
+})
+
+const boatImg = document.querySelector('.inverse-content .img-content img')
+document.addEventListener('dblclick', () => {
+    boatImg.style.transform = 'rotateX(180deg)'
+})
+
+const boatImg2 = document.querySelector('.content-destination')
+input.addEventListener('focus', () => {
+    boatImg2.remove()
+})
+const footer = document.querySelector('footer')
+window.addEventListener('resize', () => {
+    footer.style.backgroundColor = 'blue'})
+
 const busImg = document.querySelector('.intro img')
 busImg.addEventListener('mouseenter', () => {
     busImg.style.filter = 'invert(100%)'
@@ -11,8 +38,8 @@ busImg.addEventListener('mouseleave', () => {
 const body = document.querySelector('body')
 document.addEventListener('keydown', () => {
 
-    return(body.style.backgroundColor == 'white' 
-    ? body.style.backgroundColor = 'orange' : body.style.backgroundColor = 'white') 
+    body.style.backgroundColor == 'white' 
+    ? body.style.backgroundColor = 'orange' : body.style.backgroundColor = 'white' 
 })
 
 const button =  document.querySelector('#dragBtn')
@@ -21,6 +48,44 @@ function onDragStart(event) {
     event.dataTransfer.setData('text/plain',event.target.id)
     event.currentTarget.style.backgroundColor = 'yellow'
 }
+
+
+const homeP = document.querySelectorAll('p')
+document.addEventListener('wheel', () => {
+    homeP.forEach(element => {
+        element.style.fontSize = '10px'
+    })
+})
+
+const header = document.querySelector('header')
+document.addEventListener('dblclick', () => {
+
+    header.style.backgroundColor = 'green'
+    
+})
+
+document.addEventListener('dblclick', () => {
+    stopPropogation()
+    body.style.backgroundColor = 'green'
+})
+
+const navLinks = document.querySelectorAll('.nav-link')
+ navLinks.forEach(element => {
+     element.addEventListener('click', (event) => { 
+    event.preventDefault()
+     })
+})
+
+const mapImg = document.querySelector('.img-content img')
+document.addEventListener('load',
+    mapImg.src ='img/croc.jpg'
+)
+
+
+
+
+
+
 
 
 
